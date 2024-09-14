dbc1e2d32344dadce4a9c3ad1fe31832962d8653
“ill3ageef” <ill3ageef@gmail.com>
2024-09-14 22:39:04 +0300

    Added db changes and boolean field

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,16 +3,10 @@ from .models import UserInfo
 
 @admin.register(UserInfo)
 class UserInfoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'fullName', 'role', 'email', 'gradeLevel', 'school', 'phone', 'cpr', 'padded_id', 'display_additional_data')
-    
+    list_display = ('id', 'fullName', 'role', 'email', 'gradeLevel', 'school', 'phone', 'padded_id', 'has_payed')
     list_filter = ('role', 'gradeLevel', 'school')
-    fields = ('fullName', 'role', 'email', 'gradeLevel', 'school', 'phone', 'cpr', 'padded_id')
+    list_editable = ("has_payed")
     
     search_fields = ('fullName', 'email', 'phone', 'cpr')
-    
-    def display_additional_data(self, obj):
-        return ', '.join([f'{key}: {value}' for key, value in obj.additional_data.items()])
-    
-    display_additional_data.short_description = 'Additional Info'
 
     ordering = ('fullName', 'role', 'gradeLevel')
