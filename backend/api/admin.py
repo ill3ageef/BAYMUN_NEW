b3c4276905d936195e45a82a41e0579b16fdcc46
ill3ageef <ill3ageef@gmail.com>
2024-09-16 15:21:59 +0000

    added has payed filter and change the default sorting

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,9 +4,9 @@ from .models import UserInfo
 @admin.register(UserInfo)
 class UserInfoAdmin(admin.ModelAdmin):
     list_display = ('id', 'fullName', 'role', 'email', 'gradeLevel', 'school', 'phone', 'padded_id', 'has_payed')
-    list_filter = ('role', 'gradeLevel', 'school')
+    list_filter = ('role', 'gradeLevel', 'school','has_payed')
     list_editable = ("has_payed",)
     
-    search_fields = ('fullName', 'email', 'phone', 'cpr')
+    search_fields = ('has_payed','id','fullName', 'email', 'phone', 'cpr')
 
     ordering = ('fullName', 'role', 'gradeLevel')
